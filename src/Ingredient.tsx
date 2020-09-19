a7ad6d874162139ad00abdf755e8697755fe516c
Ian Kim <iansjk@gmail.com>
2020-09-19 16:34:27 -0400

    Show images for ingredients

--- src/Ingredient.tsx
+++ src/Ingredient.tsx
@@ -1,14 +1,54 @@
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
 import React from "react";
-import { Ingredient as IngredientProps } from "./materials";
+import slugify from "slugify";
+import MATERIALS, { Ingredient as IngredientProps } from "./materials";
+
+const imageSize = 95;
+const backgroundSize = imageSize + 5;
+
+const useStyles = makeStyles({
+  ingredientBackground: {
+    width: backgroundSize,
+    height: backgroundSize,
+    backgroundSize: `${backgroundSize}px ${backgroundSize}px`,
+    position: "relative",
+  },
+  ingredientImage: {
+    width: imageSize,
+    height: imageSize,
+    objectFit: "contain",
+  },
+});
 
 export default function Ingredient(props: IngredientProps): React.ReactElement {
   const { name, quantity } = props;
+  const classes = useStyles();
+  const materialData = MATERIALS[name];
   return (
-    <Box>
-      <span>
-        {name} {`x${quantity}`}
-      </span>
-    </Box>
+    <>
+      <div
+        className={classes.ingredientBackground}
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/images/item-bgs/tier${materialData.tier}.png)`,
+        }}
+      >
+        <Box
+          display="flex"
+          position="absolute"
+          width="100%"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <img
+            className={classes.ingredientImage}
+            src={`${process.env.PUBLIC_URL}/images/items/${slugify(name, {
+              lower: true,
+            })}.png`}
+            alt={name}
+          />
+        </Box>
+      </div>
+    </>
   );
 }
