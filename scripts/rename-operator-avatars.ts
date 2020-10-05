9c025a43239bbbbcd2c75e0a7790677bd2ee171c
Ian Kim <iansjk@gmail.com>
2020-10-05 18:00:33 -0400

    Add script to rename operatoravatars
    Update automatically using
    $ yarn run update:images

---
+++ scripts/rename-operator-avatars.ts
@@ -0,0 +1,49 @@
+/* eslint-disable import/prefer-default-export */
+import path from "path";
+import { promises as fs } from "fs";
+import slugify from "slugify";
+
+const ACESHIP_BASEDIR = path.join(__dirname, "../aceship");
+const OPERATOR_IMAGE_DIR = path.join(
+  __dirname,
+  "..",
+  "public",
+  "images",
+  "operators"
+);
+const avatarImageRegex = /(?<internalName>char_\d{3}_[a-z]+)(?:_(?<eliteLevel>[12])\+?)?\.png/;
+
+(async () => {
+  const characterData = await import(
+    path.join(ACESHIP_BASEDIR, "json", "tl-char.json")
+  );
+  const lookup = Object.fromEntries(
+    Object.keys(characterData).map((internalName) => [
+      internalName,
+      characterData[internalName].en,
+    ])
+  );
+  const sourceDir = path.join(ACESHIP_BASEDIR, "img", "avatars");
+  const imageFiles = await fs.readdir(sourceDir);
+  const copyImages = imageFiles
+    .filter((filename) => {
+      const match = filename.match(avatarImageRegex);
+      return match?.groups?.internalName && lookup[match?.groups?.internalName];
+    })
+    .map((filename) => {
+      const match = filename.match(avatarImageRegex);
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const newName = lookup[match!.groups!.internalName!];
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const { eliteLevel } = match!.groups!;
+      const newFilename = slugify(
+        eliteLevel ? `${newName} elite ${eliteLevel}.png` : `${newName}.png`,
+        { lower: true }
+      );
+      return fs.copyFile(
+        path.join(sourceDir, filename),
+        path.join(OPERATOR_IMAGE_DIR, newFilename)
+      );
+    });
+  await Promise.all(copyImages);
+})();
--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,17 @@
 /* eslint-disable import/prefer-default-export */
+import slugify from "slugify";
+
 const operatorImageDir = `${process.env.PUBLIC_URL}/images/operators`;
 export const getOperatorImagePath = (
   name: string,
   eliteLevel?: number
 ): string => {
+  let filename = `${slugify(name, { lower: true })}.png`;
   if (name === "Amiya" && eliteLevel === 1) {
-    return `${operatorImageDir}/Amiya Elite 1.png`;
+    filename = `${slugify(`${name} elite 1`, { lower: true })}.png`;
   }
   if (eliteLevel === 2) {
-    return `${operatorImageDir}/${name} Elite 2.png`;
+    filename = `${slugify(`${name} elite 2`, { lower: true })}.png`;
   }
-  return `${operatorImageDir}/${name}.png`;
+  return `${operatorImageDir}/${filename}`;
 };
